e3107ec9a3313a449116ab977ad0eaa821994d3e
Laakso Benita <s1900165@edu.bc.fi>
2019-04-03 21:12:59 +0300

    First homework

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
+//props= properties in the function when it is bein used
+function BoxPeople(props){
+return ( <div class="girls">
+  <h1>{props.name} </h1>
+  <p> Age:{props.age}</p>
+</div>);
+}
 
-
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let people= <div>
+  <BoxPeople name="Mari" age="29"/>
+  <BoxPeople  name= "Anne"  age="25"/>
+  <BoxPeople name= "Kati" age="45"/>
+</div>
+// The ReactDOM.render  is a function, that acsepts 2 arguments.
+// the variable and its destination
+ReactDOM.render(people,document.getElementById('root'));
